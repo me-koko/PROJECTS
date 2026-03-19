1bfe2aa3f9678ace2558c33a58a95868f53296da
Komal <komal100313@gmail.com>
2026-03-20 00:50:53 +0530

    clean commit without env

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,22 +1,17 @@
-import type { ReactNode } from "react";
+import type { PropsWithChildren } from "react";
+import Header from "./Header";
 
-type LayoutProps = {
-  children: ReactNode;
-};
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = ({ children }: PropsWithChildren<{}>) => {
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-br from-background to-muted">
-      <header>
-        header
-      </header>
-
-      <main className="flex-1 container mx-auto px-4 py-8">
+    <div className="min-h-screen flex flex-col bg-gradient-to-br from-background to-gray-900">
+      <Header/>
+      <main className="min-h-screen container mx-auto px-4 py-8">
         {children}
       </main>
 
-      <footer className="border-t backdrop-blur py-12 supports-[backdrop filter]:bg-background/60">
-        <div className="container mx-auto px-4 py-4 text-center text-gray-400">
+      <footer className="border-t backdrop-blur py-9 supports-[backdrop filter]:bg-background/60">
+        <div className="container mx-auto px-4 text-center text-gray-400">
           <p>Made with love by Komal</p>
         </div>
       </footer>
